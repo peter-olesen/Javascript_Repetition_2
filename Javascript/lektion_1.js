d3648aec3d8be314e21a31dd5ad27115bcc07f7e
kasperfryd <kfry@techcollege.dk>
2024-07-30 16:39:57 +0200

    initial commit

---
+++ Javascript/lektion_1.js
@@ -0,0 +1,43 @@
+/****************************************************
+ * LEKTION 1 - Variabler, events og conditionals    *
+ ****************************************************/
+
+// Opgave 1
+// Du skal i denne opgave redegøre for forksellen mellem en let, var og const.
+// Det vil sige at du skal illustrere hvad der sker når man forsøger at ændre en
+// var, en let og en const. Du skal bruge console.log til at demonstrere dette.
+//_______________________________________________________________________________
+
+// Opgave 2
+// I denne opgave skal du bruge aritmetik til at lave nogle regnestykker.
+// Du skal løse følgende:
+// 992 + 237 = ?
+// 44 - 12 = ?
+// 142 * 23 = ?
+// 94 / 23  = ?
+// Svarene skal ligesom opgaven ovenfor vises i hver sin console.log
+//_______________________________________________________________________________
+
+// Opgave 3
+// Du skal nu lave en click counter med et onclick event. Din click counter skal have en knap
+// der gennem Javascript får tildelt en eventlistener. Antal af clicks skal vises i DOM´en.
+//_______________________________________________________________________________
+
+// Opgave 4
+// I denne opgave skal du indele en side i fire lige store firkanter. Du skal herefter bruge en eventlistener til at
+// måle om brugeren fører musen ind over hver af de fire firkanter. Når musen rammer en firkant skal den skifte farve til en
+// tilfældigt genereret farve.
+//_________________________________________________________________________________
+
+// Opgave 5
+// I denne opgave skal du lave et input felt med validering. Du skal bruge en eventlistener til at måle på om brugeren
+// har indtastet mere end 8 bogstaver. Hvis ikke brugeren har indtastet mere en 8 bogstaver skal der vises en fejl besked
+// under input feltet, der beder brugeren om at indtaste mindst 8 bogstaver.
+//_________________________________________________________________________________
+
+// Opgave 6
+// I denne opgave skal du arbejde videre på indput feltet fra opgave 5. Du skal nu tilføje følgende betingelser til valideringen.
+//  - Brugeren skal indtaste et stort bogstav.
+//  - Brugeren skal indtaste et tegn som ikke er et bogstav.
+//  - Brugeren må ikke indtaste en string som er længere end 20 karakterer.
+//_________________________________________________________________________________
