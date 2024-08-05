9b88d962cdb47bb6986d211e4ffdc9ab23049731
tcaa-web <kfry@techcollege.dk>
2024-08-05 08:44:41 +0200

    update

--- Javascript/lektion_1.js
+++ Javascript/lektion_1.js
@@ -24,19 +24,19 @@
 //_______________________________________________________________________________
 
 // Opgave 4
-// I denne opgave skal du indele en side i fire lige store firkanter. Du skal herefter bruge en eventlistener til at
-// måle om brugeren fører musen ind over hver af de fire firkanter. Når musen rammer en firkant skal den skifte farve til en
-// tilfældigt genereret farve.
-//_________________________________________________________________________________
-
-// Opgave 5
 // I denne opgave skal du lave et input felt med validering. Du skal bruge en eventlistener til at måle på om brugeren
 // har indtastet mere end 8 bogstaver. Hvis ikke brugeren har indtastet mere en 8 bogstaver skal der vises en fejl besked
 // under input feltet, der beder brugeren om at indtaste mindst 8 bogstaver.
 //_________________________________________________________________________________
 
+// Opgave 5
+// I denne opgave skal du indele en side i fire lige store firkanter. Du skal herefter bruge en eventlistener til at
+// måle om brugeren fører musen ind over hver af de fire firkanter. Når musen rammer en firkant skal den skifte farve til en
+// tilfældigt genereret farve.
+//_________________________________________________________________________________
+
 // Opgave 6
-// I denne opgave skal du arbejde videre på indput feltet fra opgave 5. Du skal nu tilføje følgende betingelser til valideringen.
+// I denne opgave skal du arbejde videre på input feltet fra opgave 5. Du skal nu tilføje følgende betingelser til valideringen.
 //  - Brugeren skal indtaste et stort bogstav.
 //  - Brugeren skal indtaste et tegn som ikke er et bogstav.
 //  - Brugeren må ikke indtaste en string som er længere end 20 karakterer.
